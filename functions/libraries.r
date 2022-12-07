8e2e514bdf2f4837cbe8a9062fb071a1464639c4
boysenberryicecream <117742315+boysenberryicecream@users.noreply.github.com>
2022-12-07 18:36:42 +0000

    Create libraries.r

---
+++ functions/libraries.r
@@ -0,0 +1,10 @@
+# required libraries 
+
+library(palmerpenguins) # for the dataset
+library(ggplot2) # for graphing
+suppressPackageStartupMessages(library(janitor)) # for data cleaning
+suppressPackageStartupMessages(library(dplyr)) # for data subsetting and cleaning
+library(tidyr) # for data organisation 
+library(ragg) # for saving files
+library(svglite) # for savng as svg
+library(ggpubr) # for equation lines on graph
